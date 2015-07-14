c530b8a9a3c07ae76b853cfb03c80334684d47b9
MR.Y <programming@yatsukouji.com>
2015-07-14 16:41:25 +0900

    before_actionまで実装

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,53 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to "/post/#{@post.id}"
+  end
+
+  def show
+    # @post = Post.find(params[:id])
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def edit
+    # @post = Post.find(params[:id])
+  end
+
+  def update
+    # @post = Post.find(params[:id])
+    @post.update_attributes(post_params)
+    redirect_to "/posts/#{@post.id}"
+  end
+
+  def destroy
+    # @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to "/"
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(
+      :title,
+      :body,
+      :category
+      )
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+end
